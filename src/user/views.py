def897d561005de2a033b37c2356126394df4c66
JunHCha <quffl9612@naver.com>
2021-12-31 17:11:46 +0900

    feat: add user auth template

--- src/user/views.py
+++ src/user/views.py
@@ -1,3 +1,19 @@
 from django.shortcuts import render
 
-# Create your views here.
+# from django.contrib.auth import login, authenticate
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework.permissions import AllowAny
+
+# from rest_framework.response import Response
+
+
+@api_view(["POST"])
+@permission_classes([AllowAny])
+def signin():
+    pass
+
+
+@api_view(["POST"])
+@permission_classes([AllowAny])
+def signup():
+    pass
